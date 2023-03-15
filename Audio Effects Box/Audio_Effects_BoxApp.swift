8adecf2401a1ee1d5385cad748b3470a163105fa
sky-zie <skylerz@umich.edu>
2023-03-15 12:42:49 -0400

    Initial Commit

---
+++ Audio Effects Box/Audio_Effects_BoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  Audio_Effects_BoxApp.swift
+//  Audio Effects Box
+//
+//  Created by Skyler Zieske on 3/15/23.
+//
+
+import SwiftUI
+
+@main
+struct Audio_Effects_BoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Audio Effects Box/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Audio Effects Box
+//
+//  Created by Skyler Zieske on 3/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
