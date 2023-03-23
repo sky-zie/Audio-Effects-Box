aeb1262c65b5b93cac993c48634d17f8159d99b1
sky-zie <skylerz@umich.edu>
2023-03-23 14:56:57 -0400

    more knobs!

--- Audio Effects Box/ContentView.swift
+++ Audio Effects Box/ContentView.swift
@@ -9,14 +9,35 @@ import SwiftUI
 import Controls
 import SocketIO
 
+var sock = SocketClientManager()
+
 struct ContentView: View {
-    @State var val : Float = 0
-    var sock = SocketClientManager()
+    
+    @State var cutoff : Float = 0
+    @State var pitch : Float = 0
+    @State var reverb: Float = 0
+    
     var body: some View {
-        ArcKnob("Filter", value: $val)
+        VStack {
+            EffectView(name: "Reverb", val: $reverb, color: .blue)
+            EffectView(name: "Filter", val: $cutoff, color: .yellow)
+            EffectView(name: "Pitch", val: $pitch, color: .red)
+        }
+    }
+}
+
+struct EffectView: View {
+    let name : String
+    @Binding var val: Float
+    let color : Color
+    
+    var body: some View {
+        // use the `val` property here
+        ArcKnob(name, value: $val)
+            .foregroundColor(color)
             .onChange(of: val, perform: {_ in
                 sock.varChange(change:
-                    EffectChange(effect : "filter", new : val)
+                    EffectChange(effect : name, new : val)
                 )
             } )
     }
