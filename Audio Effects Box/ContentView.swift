5a298e22a533ee918d331af478b1ce050359e0c5
sky-zie <skylerz@umich.edu>
2023-04-20 14:56:57 -0400

    demo day version!

--- Audio Effects Box/ContentView.swift
+++ Audio Effects Box/ContentView.swift
@@ -10,39 +10,163 @@ import Controls
 
 
 struct ContentView: View {
-    let sock = SocketClientManager()
-    @State var cutoff : Float = 0
-    @State var pitch : Float = 0
-    @State var reverb: Float = 0
-    @State var pitchBend: Float = 0
+    @StateObject var sock = SocketClientManager()
+    @State var freq: Float = 0
+    @State var pitch : Int = 0
+    @State var curr : String = "none"
+    
+    @State var order : String = ""
+    @State var h_freq : String = "1000"
+    @State var l_freq : String = "100"
+    let pitches = ["-2", "-1", "0", "1", "2"]
     
     var body: some View {
-        VStack {
-            EffectView(name: "Reverb", val: $reverb, color: .blue, sock: sock)
-            EffectView(name: "Filter", val: $cutoff, color: .yellow, sock: sock)
-            EffectView(name: "Pitch", val: $pitch, color: .red, sock: sock)
+        VStack (alignment: .leading, spacing: 40){
+            EffectKnob(name: "sine", temp: $freq, sock: sock, current: $curr)
+                .frame(height: 300)
+            EffectSlider(name : "pitch", ind: $pitch, labels: pitches, sock: sock, current: $curr)
+                .frame(height: 30)
+            FilterBuilder(sock: sock, order: $order, high: $h_freq, low: $l_freq, current: $curr)
+            ConnectionMessage(sock: sock)
+            
         }
+        .onAppear {
+         sock.connect()
+       }
+        
     }
 }
 
-struct EffectView: View {
+struct EffectKnob: View {
     let name : String
-    @Binding var val: Float
-    let color : Color
-    let sock : SocketClientManager
+    @Binding var temp: Float
+    @State var curr: Float = 100.0
+    let color : Color = .gray
+    var sock : SocketClientManager
+    @Binding var current : String
+    let step_size = Float(100.0)
+    
     
     var body: some View {
         // use the `val` property here
-        ArcKnob(name, value: $val)
-            .foregroundColor(color)
-            .onChange(of: val, perform: {_ in
+        ArcKnob(name, value: $temp, range: 100...1000)
+            .foregroundColor((current == name) ? Color.green : Color.white)
+            .onChange(of: temp, perform: {_ in
+                if ((curr - temp) >= step_size || (curr - temp) <= (-1 * step_size)){
+                    print("curr \(curr)")
+                    curr = temp
+                    sock.varChange(data:
+                        EffectData(event : "change", effect : name, new : String(temp))
+                    )
+                }
+                current = name
+                    
+            } )
+        }
+}
+
+struct EffectSlider: View {
+    let name : String
+    @Binding var ind: Int
+    let labels: [String]
+    var sock : SocketClientManager
+    let color : Color = .gray
+    @Binding var current : String
+    
+    var body: some View {
+        // use the `val` property here
+        IndexedSlider(index: $ind, labels: labels)
+            .foregroundColor((current == name) ? Color.green : Color.white)
+            .backgroundColor(.black.opacity(0.1))
+            .cornerRadius(500)
+            .onChange(of: ind, perform: {_ in
                 sock.varChange(data:
-                    EffectData(event : "change", effect : name, new : val)
+                    EffectData(event : "change", effect : "pitch", new : labels[ind])
                 )
+                current = name
             } )
     }
 }
 
+struct ConnectionMessage : View {
+    var sock: SocketClientManager
+    let padding : CGFloat? = 0
+    let connect_str : String
+    init(sock: SocketClientManager) {
+        self.sock = sock
+        if (self.sock.isConnected) { self.connect_str = "Connected!" }
+        else {
+            self.connect_str = "    Not Connected..."
+        }
+    }
+    var body: some View {
+        HStack {
+            Text(connect_str)
+                .padding(.top, padding)
+            
+            if (!self.sock.isConnected){
+                Spacer()
+                Button(action: {self.sock.connect()}) {
+                    Text("Connect")
+                }
+                .buttonStyle(.bordered)
+            }
+        }
+    }
+}
+
+struct FilterBuilder : View {
+    let name : String = "filter"
+    var sock: SocketClientManager
+    @Binding var order :  String
+    @Binding var high : String
+    @Binding var low : String
+    @Binding var current : String
+    
+    var init_vals = [0, 0, 0]
+    @FocusState private var nameIsFocused
+    @State private var filterType = "band"
+    let filters = ["band", "low", "high"]
+
+    var body: some View {
+        VStack {
+            HStack (spacing: 3) {
+                TextField("Order", text: $order)
+                if (filterType != "high") {
+                    TextField("High Freq (Hz) ", text: $high)
+                }
+                if (filterType != "low") {
+                    TextField("Low Freq (Hz) ", text: $low)
+                }
+            }.keyboardType(.numberPad)
+              .focused($nameIsFocused)
+              .foregroundColor((current == name) ? Color.green : Color.gray)
+            HStack {
+                Button(action: {
+                    if ((Int(low) ?? 0) >= 5) { sock.filterBuild(data:
+                        FilterData(type: filterType, order : order, high : high, low : low)
+                    ) }
+                    current = name
+                    nameIsFocused = false
+                    
+                }) {
+                    Text("Build")
+                }
+                .foregroundColor((current == name) ? Color.green : Color.white)
+                
+                Picker("text", selection: $filterType) {
+                    ForEach(filters, id: \.self) {
+                        Text($0)
+                    }
+                }
+                .pickerStyle(.menu)
+                .accentColor((current == name) ? Color.green : Color.white)
+            }
+        }.textFieldStyle(.roundedBorder)
+         .buttonStyle(.bordered)
+    }
+}
+    
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
@@ -52,5 +176,13 @@ struct ContentView_Previews: PreviewProvider {
 struct EffectData {
     let event: String
     let effect: String
-    let new: Float
+    let new: String
 }
+
+struct FilterData {
+    let type : String
+    let order: String
+    let high: String
+    let low: String
+}
+
--- Audio Effects Box/Socket.swift
+++ Audio Effects Box/Socket.swift
@@ -8,54 +8,99 @@
 import Foundation
 import Starscream
 
-
-class SocketClientManager{
-    var socket : WebSocket
-    var isConnected : Bool
+class SocketClientManager: ObservableObject, WebSocketDelegate {
+    @Published private var socket : WebSocket
+    @Published var isConnected : Bool = false
     init() {
-        var request = URLRequest(url: URL(string: "http://172.20.10.2:8000/")!)
+        var request = URLRequest(url: URL(string: "ws://172.20.10.8:5000")!)
         request.timeoutInterval = 5
-        self.socket = WebSocket(request: request)
-        self.isConnected = false
-        setupSocketEvents()
-        self.socket.connect()
-        self.socket.write(string: "Hi Server!")
-        self.socket.delegate =
+        let pinner = FoundationSecurity(allowSelfSigned: true) // don't validate SSL certificates
+        socket = WebSocket(request: request, certPinner: pinner)
+        socket.delegate = self
+        print("Attempting to connect to server...")
+    }
+   
+    func connect() {
+        socket.connect()
+        print("did it work?")
+    }
+    func varChange(data: EffectData) {
+        let myString = data.new
+        socket.write(string: myString)
     }
     
-    func setupSocketEvents() {
-        self.socket.onEvent = { event in
-            switch event {
-                case .connected(let headers):
-                    self.isConnected = true
-                    print("websocket is connected: \(headers)")
-                case .disconnected(let reason, let code):
-                    self.isConnected = false
-                    print("websocket is disconnected: \(reason) with code: \(code)")
-                case .text(let string):
-                    print("Received text: \(string)")
-                case .binary(let data):
-                    print("Received data: \(data.count)")
-                case .ping(_):
-                    break
-                case .pong(_):
-                    break
-                case .viabilityChanged(_):
-                    break
-                case .reconnectSuggested(_):
-                    break
-                case .cancelled:
-                    self.isConnected = false
-                case .error(let error):
-                    self.isConnected = false
-                    print("error: \(error)")
-            }
+    func filterBuild(data: FilterData) {
+        socket.write(string: "f/\(data.type)/\(data.order)/\(data.low)/\(data.high)")
+    }
+    
+    func didConnect(socket: WebSocket) {
+        isConnected = true
+        print("WebSocket connected")
+    }
+    
+    func didDisconnect(socket: WebSocket, error: Error?) {
+        isConnected = false
+        if let error = error {
+            print("WebSocket disconnected due to error: \(error.localizedDescription)")
+        } else {
+            print("WebSocket disconnected")
         }
     }
     
+    func didReceive(event: WebSocketEvent, client: WebSocket) {
+        switch event {
+        case .connected(let headers):
+            isConnected = true
+            print("websocket is connected: \(headers)")
+        case .disconnected(let reason, let code):
+            isConnected = false
+            print("websocket is disconnected: \(reason) with code: \(code)")
+        case .text(let string):
+            print("Received text: \(string)")
+        case .binary(let data):
+            print("Received data: \(data.count)")
+        case .ping(_):
+            break
+        case .pong(_):
+            break
+        case .viabilityChanged(_):
+            break
+        case .reconnectSuggested(_):
+            break
+        case .cancelled:
+            isConnected = false
+        case .error(let error):
+            print("WebSocket error: \(String(describing: error?.localizedDescription))")
+        }
+    }
     
-    func varChange(data: EffectData) {
-        self.socket.write(string: "\(data.event)/\(data.effect)/\(data.new)")
-        //print("should have written?")
+    func websocketDidConnect(socket: WebSocketClient) {
+        isConnected = true
+        print("WebSocket connected")
+    }
+    
+    func websocketDidDisconnect(socket: WebSocketClient, error: Error?) {
+        isConnected = false
+        if let error = error {
+            print("WebSocket disconnected due to error: \(error.localizedDescription)")
+        } else {
+            print("WebSocket disconnected")
+        }
+    }
+    
+    func websocketDidReceiveMessage(socket: WebSocketClient, text: String) {
+        print("Received text: \(text)")
+    }
+    
+    func websocketDidReceiveData(socket: WebSocketClient, data: Data) {
+        print("Received data: \(data.count)")
+    }
+    
+    func websocketDidReceivePong(socket: WebSocketClient, data: Data?) {
+        print("Received pong")
+    }
+    
+    func websocketShouldConvertTextFrame(toString: String) -> Bool {
+        return true
     }
 }
